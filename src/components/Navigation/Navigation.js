3f708a3bbca405b2a2a2cb12cad09e176ba191f2
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-02 16:18:17 -0500

    create Navigation component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <button className="navigation__register-btn">SignUp</button>
+      <button className="navigation__login-btn">Log In</button>
+      <a className="navigation__about" href="#">
+        About
+      </a>
+    </div>
+  );
+};
+
+export default Navigation;
