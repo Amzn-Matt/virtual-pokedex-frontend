97f846ff71ea6e5e05fb912b3feb7e3a09253caa
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-03 17:15:39 -0500

    Create about component

---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <section id="about-library" className="about">
+      <h2 className="about__title">About the author</h2>
+      <div className="about__content">
+        <p className="about__paragraph">
+          Libraries are essential establishments in practically every city, and
+          Practicum's library is no exception. It first opened its doors way
+          back in 2018, right as our humble town began bidding welcome to its
+          very first citizens!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
