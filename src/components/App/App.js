94e5114a22946ef26c59cfe03d9885780d9c5ba1
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-03 17:17:29 -0500

    Import Main and Footer components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "../Header/Header.js";
 import Main from "../Main/Main.js";
+import Footer from "../Footer/Footer.js";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <Main />
-    </>
+      <Footer />
+    </div>
   );
 }
 
