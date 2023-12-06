2fb5314cc18a8a26568bf93ba4d119fa49b08b48
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-05 20:14:25 -0500

    fetch pokeApi data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,24 +2,63 @@ import "./App.css";
 import Header from "../Header/Header.js";
 import Main from "../Main/Main.js";
 import Footer from "../Footer/Footer.js";
+import Profile from "../Profile/Profile.js";
 import { useEffect, useState } from "react";
-import { getAllPokemon } from "../../utils/PokeApi.js";
+import { getAllPokemon, getPokemon } from "../../utils/PokeApi.js";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  const [pokemonItems, setPokemonItems] = useState({});
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [search, setSearch] = useState("");
+  const baseUrl = "https://pokeapi.co/api/v2/pokemon";
+
+  const fetchData = async () => {
+    const response = await getAllPokemon(baseUrl);
+    console.log(response);
+    setNextUrl(response.next);
+    loadingPokemon(response.results);
+  };
+
+  const loadingPokemon = async (data) => {
+    const pokemon = await Promise.all(
+      data.map(async (pokemon) => {
+        const pokemonObj = await getPokemon(pokemon.url);
+        return pokemonObj;
+      })
+    );
+    setPokemonData(pokemon);
+  };
+
+  const handleShowMore = async () => {
+    const showMore = await getAllPokemon(nextUrl);
+    await loadingPokemon(showMore.results);
+    setNextUrl(showMore.next);
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
-    getAllPokemon().then((data) => {
-      setPokemonItems(data.results);
-    });
+    fetchData();
   }, []);
-
-  // console.log(pokemonItems);
 
   return (
     <div className="app">
       <Header />
-      <Main pokemonItems={pokemonItems} />
+      <Switch>
+        <Route exact path="/">
+          <Main
+            pokemonData={pokemonData}
+            onShowMore={handleShowMore}
+            onChange={handleChange}
+          />
+        </Route>
+        <Route path="/profile">
+          <Profile pokemonData={pokemonData} />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
