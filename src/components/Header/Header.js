49983cbb22cd3291f47c312b92b8d1218dcb539e
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-05 20:14:07 -0500

    all link to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import "./Header.css";
 import Navigation from "../Navigation/Navigation";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header__title">Virtual Pokèdex</h1>
+      <Link to="/">
+        <h1 className="header__title">Virtual Pokèdex</h1>
+      </Link>
       <Navigation />
     </header>
   );
