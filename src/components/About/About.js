adc9e900fb7f6aea5f7cd43736dcbde15c576fd5
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-10 16:37:21 -0500

    refactor about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,24 @@
 import "./About.css";
 
-const About = () => {
+const About = ({ isLoading }) => {
   return (
     <section id="about" className="about">
-      <h2 className="about__title">About the author</h2>
-      <div className="about__content">
-        <p className="about__paragraph">
-          Libraries are essential establishments in practically every city, and
-          Practicum's library is no exception. It first opened its doors way
-          back in 2018, right as our humble town began bidding welcome to its
-          very first citizens!
-        </p>
-      </div>
+      {isLoading ? (
+        <div></div>
+      ) : (
+        <>
+          {" "}
+          <h2 className="about__title">About the author</h2>
+          <div className="about__content">
+            <p className="about__paragraph">
+              Hello my name is Matthew Sanchez I'm a sofware developer. I enjoy
+              sports video games and music. This project serves as a love letter
+              to a gaming franchise I grew up with by implementing my skills as
+              a developer.
+            </p>
+          </div>
+        </>
+      )}
     </section>
   );
 };
