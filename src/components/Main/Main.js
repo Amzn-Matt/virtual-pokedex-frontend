65534884b2615517ffe5c12a89f8dddf5760053a
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-10 16:39:45 -0500

    add preloader to main and pokemon page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,38 +1,52 @@
 import "./Main.css";
 import ItemCard from "../ItemCard/ItemCard";
-import About from "../About/About";
-const Main = ({ pokemonData, onNextPage, onPreviousPage, prevUrl, nexUrl }) => {
+import Preloader from "../Preloader/Preloader";
+
+const Main = ({
+  pokemonData,
+  onNextPage,
+  onPreviousPage,
+  prevUrl,
+  nexUrl,
+  isLoading,
+}) => {
   return (
     <main className="main">
       <input className="form__search-input"></input>
       <section className="cards">
-        <ul className="card__list">
-          {pokemonData.map((pokemon, i) => {
-            return <ItemCard key={i} pokemon={pokemon} />;
-          })}
-        </ul>
-        <div className="card__button-wrapper">
-          {prevUrl && (
-            <button
-              className="card__list-btn"
-              type="button"
-              onClick={onPreviousPage}
-            >
-              Previous Page
-            </button>
-          )}
-          {!nexUrl && (
-            <button
-              className="card__list-btn"
-              type="button"
-              onClick={onNextPage}
-            >
-              Next Page
-            </button>
-          )}
-        </div>
+        {isLoading ? (
+          <Preloader />
+        ) : (
+          <>
+            {" "}
+            <ul className="card__list">
+              {pokemonData.map((pokemon, i) => {
+                return <ItemCard key={i} pokemon={pokemon} />;
+              })}
+            </ul>
+            <div className="card__button-wrapper">
+              {prevUrl && (
+                <button
+                  className="card__list-btn"
+                  type="button"
+                  onClick={onPreviousPage}
+                >
+                  Previous Page
+                </button>
+              )}
+              {!nexUrl && (
+                <button
+                  className="card__list-btn"
+                  type="button"
+                  onClick={onNextPage}
+                >
+                  Next Page
+                </button>
+              )}
+            </div>
+          </>
+        )}
       </section>
-      <About />
     </main>
   );
 };
--- src/components/PokemonPage/PokemonPage.js
+++ src/components/PokemonPage/PokemonPage.js
@@ -4,8 +4,9 @@ import { useEffect, useState } from "react";
 import { TYPE_COLOR } from "../../utils/Constants";
 import { TfiRuler } from "react-icons/tfi";
 import { LiaWeightHangingSolid } from "react-icons/lia";
+import Preloader from "../Preloader/Preloader";
 
-const PokemonPage = () => {
+const PokemonPage = ({ isLoading }) => {
   const params = useParams();
   const [pokemonId, setPokemonId] = useState({});
   const [pokemonDetails, setPokemonDetails] = useState({});
@@ -60,85 +61,95 @@ const PokemonPage = () => {
 
   return (
     <main className="pokemon__profile">
-      <div className="pokemon__info-container">
-        <h4 className="pokemon__index">Pokemon #{pokemonId.order}</h4>
-        <h2 className="pokemon__name">{pokemonId.name}</h2>
-        <div className="pokemon__types-wrapper">
-          {pokemonId?.types?.map((type, i) => {
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <>
+          {" "}
+          <div className="pokemon__info-container">
+            <h4 className="pokemon__index">Pokemon #{pokemonId.order}</h4>
+            <h2 className="pokemon__name">{pokemonId.name}</h2>
+            <div className="pokemon__types-wrapper">
+              {pokemonId?.types?.map((type, i) => {
+                return (
+                  <span
+                    style={{
+                      backgroundColor: `${TYPE_COLOR[type.type.name]}`,
+                    }}
+                    className="pokemon__types"
+                    key={i}
+                  >
+                    {type.type.name}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+          <img
+            className="pokemon__image"
+            src={IMAGE_URLS[selectedImage]}
+            alt={pokemonId.name}
+          />
+          <div className="pokemon__image-btn-wrapper">
+            {imageVariants.map((image, i) => {
+              return (
+                <button
+                  key={i}
+                  className="image__button"
+                  style={{
+                    backgroundColor: `${
+                      selectedImage === image ? "#2f71e5" : "#d3d3d3"
+                    }`,
+                    color: "white",
+                  }}
+                  onClick={() => setSelectedImage(image)}
+                >
+                  {image}
+                </button>
+              );
+            })}
+          </div>
+          <span className="pokemon__genus">{species}</span>
+          <span className="pokemon__entry">{description}</span>
+          <div className="pokemon__details-container">
+            <span className="pokemon__weight">
+              <LiaWeightHangingSolid />
+              {(Math.round(pokemonId.weight * 0.220462 + 0.0001) * 100) / 100}
+              lbs
+            </span>
+            <span className="pokemon__height">
+              <TfiRuler />
+              {Math.round(
+                ((pokemonId.height * 0.3280839895 + 0.0001) * 100) / 100
+              )}
+              ft
+            </span>
+          </div>
+          <div className="pokemon__abilities-container">
+            Abilities:
+            {pokemonId?.abilities?.map((ability, i) => {
+              return (
+                <span className="pokemon__ability" key={i}>
+                  {ability.ability.name}
+                </span>
+              );
+            })}
+          </div>{" "}
+          {pokemonId.stats?.map((stat, i) => {
             return (
-              <span
-                style={{
-                  backgroundColor: `${TYPE_COLOR[type.type.name]}`,
-                }}
-                className="pokemon__types"
-                key={i}
-              >
-                {type.type.name}
-              </span>
+              <div key={i} className="pokemon__stats-container">
+                <span className="pokemon__stat-name"> {stat.stat.name} </span>
+                <progress
+                  value={stat.base_stat}
+                  max={130}
+                  className="pokemon__progress"
+                ></progress>
+                <span>{stat.base_stat}</span>
+              </div>
             );
           })}
-        </div>
-      </div>
-      <img
-        className="pokemon__image"
-        src={IMAGE_URLS[selectedImage]}
-        alt={pokemonId.name}
-      />
-      <div className="pokemon__image-btn-wrapper">
-        {imageVariants.map((image, i) => {
-          return (
-            <button
-              key={i}
-              className="image__button"
-              style={{
-                backgroundColor: `${
-                  selectedImage === image ? "#2f71e5" : "#d3d3d3"
-                }`,
-                color: "white",
-              }}
-              onClick={() => setSelectedImage(image)}
-            >
-              {image}
-            </button>
-          );
-        })}
-      </div>
-      <span className="pokemon__genus">{species}</span>
-      <span className="pokemon__entry">{description}</span>
-      <div className="pokemon__details-container">
-        <span className="pokemon__weight">
-          <LiaWeightHangingSolid />
-          {(Math.round(pokemonId.weight * 0.220462 + 0.0001) * 100) / 100}lbs
-        </span>
-        <span className="pokemon__height">
-          <TfiRuler />
-          {Math.round(((pokemonId.height * 0.3280839895 + 0.0001) * 100) / 100)}
-          ft
-        </span>
-      </div>
-      <div className="pokemon__abilities-container">
-        Abilities:
-        {pokemonId?.abilities?.map((ability, i) => {
-          return (
-            <span className="pokemon__ability" key={i}>
-              {ability.ability.name}
-            </span>
-          );
-        })}
-      </div>{" "}
-      {pokemonId.stats?.map((stat, i) => {
-        return (
-          <div key={i} className="pokemon__stats-container">
-            <span className="pokemon__stat-name"> {stat.stat.name} </span>
-            <progress
-              value={stat.base_stat}
-              max={130}
-              className="pokemon__progress"
-            ></progress>
-            <span>{stat.base_stat}</span>
-          </div>
-        );
-      })}
+        </>
+      )}
     </main>
   );
 };
