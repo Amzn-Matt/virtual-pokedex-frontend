904467bc0b469b66577196c3e34307aa90445a9c
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-13 00:14:56 -0500

    fix mobile modal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,17 @@ import "./Header.css";
 import Navigation from "../Navigation/Navigation";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const Header = ({ onSignUp, onLogin }) => {
+const Header = ({ onSignUp, onLogin, onMobileBtn }) => {
   return (
     <header className="header">
       <Link to="/">
         <h1 className="header__title">Virtual Pokèdex</h1>
       </Link>
-      <Navigation onSignUp={onSignUp} onLogin={onLogin} />
+      <Navigation
+        onSignUp={onSignUp}
+        onLogin={onLogin}
+        onMobileBtn={onMobileBtn}
+      />
     </header>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,6 @@
-const Navigation = ({ onSignUp, onLogin }) => {
+import { VscThreeBars } from "react-icons/vsc";
+
+const Navigation = ({ onSignUp, onLogin, onMobileBtn }) => {
   return (
     <div className="navigation">
       <button className="navigation__register-btn" onClick={onSignUp}>
@@ -6,6 +8,9 @@ const Navigation = ({ onSignUp, onLogin }) => {
       </button>
       <button className="navigation__login-btn" onClick={onLogin}>
         Log In
+      </button>
+      <button className="navigation__mobile-btn" onClick={onMobileBtn}>
+        <VscThreeBars className="mobile__icon" />
       </button>
     </div>
   );
