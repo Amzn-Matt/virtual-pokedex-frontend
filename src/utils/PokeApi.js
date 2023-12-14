48ece348356c5cbc201763aa7b0d642ce8251c3c
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-14 15:21:32 -0500

    add pokemon stats and species api call

--- src/utils/PokeApi.js
+++ src/utils/PokeApi.js
@@ -18,6 +18,26 @@ export async function getPokemon(url) {
   });
 }
 
+export async function getPokemonSpecies(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
+
+export async function getPokemonStats(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
+
 export async function getGlobalPokemon(url) {
   return new Promise((resolve, reject) => {
     fetch(url)
