12d147648fec38c16a42e975de587742adbb8dc0
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-14 14:14:33 -0500

    create NotFound component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <section>
+      <h3 className="no__results">No Results</h3>
+    </section>
+  );
+};
+
+export default NotFound;
