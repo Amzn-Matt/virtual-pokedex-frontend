17cef2a22ed9ba05dea4a7ee95c4ecfc45dcacbf
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-12-20 21:55:40 -0500

    uncomment pokemon page fetch requets

--- src/utils/PokeApi.js
+++ src/utils/PokeApi.js
@@ -1,54 +1,54 @@
 // import { useEffect } from "react";
 
-// export async function getAllPokemon(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         resolve(data);
-//       });
-//   });
-// }
+export async function getAllPokemon(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
 
-// export async function getPokemon(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         resolve(data);
-//       });
-//   });
-// }
+export async function getPokemon(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
 
-// export async function getPokemonSpecies(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         resolve(data);
-//       });
-//   });
-// }
+export async function getPokemonSpecies(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
 
-// export async function getPokemonStats(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         resolve(data);
-//       });
-//   });
-// }
+export async function getPokemonStats(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
 
-// export async function getGlobalPokemon(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         resolve(data);
-//       });
-//   });
-// }
+export async function getGlobalPokemon(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+}
 
 // export async function getGlobalPokemonStats(url) {
 //   return new Promise((resolve, reject) => {
