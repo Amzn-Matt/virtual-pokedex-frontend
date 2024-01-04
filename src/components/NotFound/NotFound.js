a526e8534a2110322d0aa7dd7221e94f2724a6a2
Matthew Sanchez <mattsanchez1215@gmail.com>
2024-01-03 22:21:21 -0500

    refactor NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,7 @@
 import "./NotFound.css";
 
 const NotFound = () => {
-  return (
-    <section>
-      <h3 className="no__results">No Results</h3>
-    </section>
-  );
+  return <h3 className="no__results">No Results</h3>;
 };
 
 export default NotFound;
