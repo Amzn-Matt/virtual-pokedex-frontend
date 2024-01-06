01536842272a1e7a3f9d284eb27494b456c6ff20
Matthew Sanchez <mattsanchez1215@gmail.com>
2024-01-06 15:42:56 -0500

    change header on route change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,22 @@
 import "./Header.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 const Header = ({ handleSearch }) => {
+  const { pathname } = useLocation();
+
   return (
-    <header className="header">
+    <header
+      className="header"
+      style={{
+        justifyContent: pathname === "/" ? "space-between" : "center",
+      }}
+    >
       <Link to="/">
         <h1 className="header__title">Virtual Pokèdex</h1>
       </Link>
-      <SearchBar handleSearch={handleSearch} />
+      {pathname === "/" ? <SearchBar handleSearch={handleSearch} /> : null}
     </header>
   );
 };
